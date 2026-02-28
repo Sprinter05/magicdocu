3c6cf3627ef4f9f0d7019ddf3b1216d247c28283
Markel Mencía <markel.mnc@gmail.com>
2026-02-28 16:12:36 +0100

    Add select file form

--- backend/core/forms.py
+++ backend/core/forms.py
@@ -3,8 +3,12 @@ from django.forms.models import ModelForm
 
 from core.models import Document
 
-
 class UploadFileForm(ModelForm):
+    class Meta:
+        model = Document
+        fields = ["file"]
+
+class SelectFileForm(ModelForm):
     class Meta:
         model = Document
         fields = ["file"]
