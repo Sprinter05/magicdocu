51d06d61037b7a22c8fc995ef554d6add9ea6080
Markel Mencía <markel.mnc@gmail.com>
2026-02-28 14:43:02 +0100

    Add select file form

--- backend/core/forms.py
+++ backend/core/forms.py
@@ -8,3 +8,9 @@ class UploadFileForm(ModelForm):
     class Meta:
         model = Document
         fields = ["file", "author", "filetype", "modified_date", "created_date", "accessed_date", "size", "tags", "shared_users"]
+
+
+class SelectFileForm(ModelForm):
+    class Meta:
+        model = Document
+        fields = ["file"]
