e47d912bf246f74787a758c56cc783e7479ee2bf
Sprinter05 <sprinterhr@gmail.com>
2026-02-28 02:19:06 +0100

    Added auth user

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class AuthUser(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    username = models.TextField(null=True, unique=True)
+
+    def __str__(self):
+        if self.username != None:
+            return self.username
+        return "?"
