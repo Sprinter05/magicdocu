50cd90d4cb2bb674b6fbf92846e672c3b7aca425
Markel Mencía <markel.mnc@gmail.com>
2026-02-28 14:43:56 +0100

    add testing file selector

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .forms import UploadFileForm
-
+from .forms import UploadFileForm, SelectFileForm
+from django.views.generic.list import ListView
+from core.models import Document
+from core.workers import *
 
 def upload_file(request):
     if request.method == "POST":
@@ -12,3 +14,12 @@ def upload_file(request):
     else:
         form = UploadFileForm()
     return render(request, "upload.html", {"form": form})
+
+def document_list(request):
+    if request.method == "POST":
+        document_id = request.POST.get("document_id")
+        document = Document.objects.get(id=document_id)
+        print(extract_text(document.file.name))
+
+    documents = Document.objects.filter(author=request.user)
+    return render(request, "document_list.html", {"documents": documents})
